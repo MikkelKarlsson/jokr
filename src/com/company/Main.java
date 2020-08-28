67e2c476c544d1422ddb24513e71bd1673af86bd
MikkelKarlsson <Mikkel.karlsson@hotmail.com>
2020-08-28 10:57:19 +0200

    Version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,42 +1,44 @@
 package com.company;
 
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
-import static java.util.Collections.shuffle;
-
-public class Main {
-
-    public static void main(String[] args) {
+public class Main
+{
+    public static void main(String[] args)
+    {
         Scanner scan = new Scanner(System.in);
+        Main m = new Main();
 
-        randomJoke();
-        input(scan, randomJoke());
+        m.displayRandom(m.getJokes(), scan);
     }
 
-    public static List randomJoke(){
-        List<String> aList = new ArrayList<>();
-        aList.add("joke1");
-        aList.add("joke2");
-        aList.add("joke3");
-        aList.add("joke4");
-        aList.add("joke5");
-        shuffle(aList);
-        return aList;
+    private ArrayList<String> getJokes()
+    {
+        ArrayList<String> jokes = new ArrayList<>();
+        jokes.add("How does a scientist freshen their breath? With experi-mints");
+        jokes.add("What did the 0 say to the 8? Nice belt.");
+        jokes.add("Knock knock.\nWho's there? \nHatch. \nHatch who? \nBless you!");
+        jokes.add("Why did the melons plan a big wedding? Because they cantaloupe!");
 
+        return jokes;
     }
 
-    public static void input(Scanner scan, List aList){
-        System.out.println("Do u wanna listen to a joke?\n1. Yes\n2. No");
-        int answer = scan.nextInt();
-        if (answer == 1){
-            System.out.println(aList.get(1));
-        } else {
-            System.out.println("Fuck u");
+    private void displayRandom(ArrayList<String> strings, Scanner scan)
+    {
+
+
+        System.out.println("Wanna listen to a joke?\nIf yes, press enter.\n");
+        scan.nextLine();
+
+        boolean flag = true;
+        while(flag) {
+            int index = new Random().nextInt(strings.size());
+            System.out.println(strings.get(index));
+            System.out.println("\nAnother one? We the best music! ENTERRRRRRRRRRR <3 ^^ O.o\n\n\n");
+            scan.nextLine();
         }
     }
 }
